852c954f7df72c156abed5a2b2d0d80a33a337a5
Olivier MacBook <olivier.granacher@gmail.com>
2020-05-03 16:02:59 +0200

    v 0.1.0

--- R/palDesaturate.R
+++ R/palDesaturate.R
@@ -3,8 +3,8 @@
 #' Palette with same hue and luminance colors, only saturation varies down from start color
 #'
 #' @param base_color hex code or name
-#' @param n number of colors to return
-#' @param end fraction of desaturation for end color
+#' @param n number of colors to return in palette
+#' @param max_desaturation fraction of desaturation for end color : 1 is complete desaturation
 #' @param lighten between -1 and 1, positive values for lightening and negtive for darkening
 #' @param plot  if TRUE return hclplot
 #'
@@ -13,8 +13,8 @@
 #' @export
 #'
 #'
-palDesaturate <- function(base_color = "blue", n = 3 , end = .8, plot = F, lighten = 0) {
-  pal <- colorspace::desaturate(rep(base_color, n), seq(0, end, length.out = n))
+palDesaturate <- function(base_color = "blue", n = 3 , max_desaturation = .8, plot = F, lighten = 0) {
+  pal <- colorspace::desaturate(rep(base_color, n), seq(0, max_desaturation, length.out = n))
   pal <- colorspace::lighten(pal, amount = lighten, method = "relative")
   if (plot) { return(colorspace::hclplot(pal))
   } else {
