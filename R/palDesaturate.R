8337434fec82c712bece0af1ccc23ae137ad6a2a
Olivier MacBook <olivier.granacher@gmail.com>
2020-05-03 11:40:06 +0200

    palDesaturate

---
+++ R/palDesaturate.R
@@ -0,0 +1,23 @@
+#' Creates palette from base color
+#'
+#' Palette with same hue and luminance colors, only saturation varies down from start color
+#'
+#' @param base_color hex code or name
+#' @param n number of colors to return
+#' @param end fraction of desaturation for end color
+#' @param lighten between -1 and 1, positive values for lightening and negtive for darkening
+#' @param plot  if TRUE return hclplot
+#'
+#'
+#' @return palette or
+#' @export
+#'
+#'
+palDesaturate <- function(base_color = "blue", n = 3 , end = .8, plot = F, lighten = 0) {
+  pal <- colorspace::desaturate(rep(base_color, n), seq(0, end, length.out = n))
+  pal <- colorspace::lighten(pal, amount = lighten, method = "relative")
+  if (plot) { return(colorspace::hclplot(pal))
+  } else {
+      return(pal)
+    }
+}
