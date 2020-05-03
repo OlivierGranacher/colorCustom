9e471995e013abd4055764c54ca0459b73651f79
Olivier MacBook <olivier.granacher@gmail.com>
2020-05-03 16:12:48 +0200

    addition of dir argument

--- R/palDesaturate.R
+++ R/palDesaturate.R
@@ -7,15 +7,22 @@
 #' @param max_desaturation fraction of desaturation for end color : 1 is complete desaturation
 #' @param lighten between -1 and 1, positive values for lightening and negtive for darkening
 #' @param plot  if TRUE return hclplot
+#' @param dir -1 for reversing color order
 #'
 #'
 #' @return palette or plot for palette
 #' @export
 #'
 #'
-palDesaturate <- function(base_color = "blue", n = 3 , max_desaturation = .8, plot = F, lighten = 0) {
+palDesaturate <- function(base_color = "blue",
+                          n = 3 ,
+                          max_desaturation = .8,
+                          plot = F,
+                          lighten = 0,
+                          dir = 1) {
   pal <- colorspace::desaturate(rep(base_color, n), seq(0, max_desaturation, length.out = n))
   pal <- colorspace::lighten(pal, amount = lighten, method = "relative")
+  if (dir == -1 ) pal <- rev(pal)
   if (plot) { return(colorspace::hclplot(pal))
   } else {
       return(pal)
